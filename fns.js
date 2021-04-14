bb4e489bfe65ee4b9ab865dc9762dd600165a81f
MatiasO123 <matiasolivieri93@gmail.com>
2021-04-13 23:41:10 -0300

    Hice función de división

--- fns.js
+++ fns.js
@@ -19,6 +19,11 @@ function mul(a, b) {}
 function div(a, b) {
     // Tener en cuenta division por 0
     // Si b es 0 retornar "Error: div by 0"
+    if(b != 0)
+        return a / b;
+    else{
+        return "El segundo número no puede ser 0 (cero)";
+    }
 }
 
 function pow(a, b) {}
